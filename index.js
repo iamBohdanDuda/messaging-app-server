9553cdff359a6c9f21724fa5953b6c60b91e34d8
Bohdan <bohdan.duda.it@gmail.com>
2023-03-18 18:45:37 +0200

    wss added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const WebSocketServer = require('ws').Server;
+const WebSocketServer = require('wss').Server;
 const wss = new WebSocketServer({ server: server });
 
 
